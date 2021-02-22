42acb9a89f2b4d1fcd943d3b2e7a7879d6197904
liamvandelangeryt1 <43748516+liamvandelangeryt1@users.noreply.github.com>
2021-02-22 16:21:40 +0100

    nav integratie

--- wordpress/wp-content/themes/Bikestore/functions.php
+++ wordpress/wp-content/themes/Bikestore/functions.php
@@ -1,19 +1,42 @@
 <?php
 
-    function cg_add_theme_scripts() {
-            wp_enqueue_style( 'screen', get_template_directory_uri() . '/css/screen.css');
-    }
-        add_action( 'wp_enqueue_scripts', 'cg_add_theme_scripts' );
 
-        add_theme_support( 'post-thumbnails' );
+function bs_laadCSSenScript()
+{
+  $pathTheme = get_template_directory_uri();
+  wp_enqueue_style("bootstrap", $pathTheme . '/css/bootstrap.min.css');
+  wp_enqueue_style('Bikestore', $pathTheme . '/css/screen.css', ['bootstrap']);
+
+  wp_enqueue_script('bootstrapjs', $pathTheme . '/script/bootstrap.min.js');
+  //wp_enqueue_script('menujs', $pathTheme . '/script/.js');
+}
+
+function wpb_custom_new_menu()
+{
+  register_nav_menus(
+    array(
+      'my-custom-menu' => __('My Custom Menu'),
+      'extra-menu' => __('Extra Menu')
+    )
+  );
+}
+
+function bs_register_my_menus()
+{
+  register_nav_menus(
+    array(
+      'main-menu' => __('Hoofd Menu'),
+      'footer-menu' => __('voet Menu')
+    )
+  );
+}
 
 
-    function cl_laadEigenCSS(){
-      $pathVanTheme = get_template_directory_uri();
-      wp_enqueue_style( "planetbucks",$pathVanTheme. '/css/screen.css' );
-    }
+add_action("wp_enqueue_scripts", "bs_laadCSSenScript");
+add_action('init', 'bs_register_my_menus');
+add_theme_support('post-thumbnails');
 
 
-            add_action('wp_enqueue_scripts', 'cg_add_theme_scripts');
-            add_theme_support('post-thumbnails');
+add_action('init', 'wpb_custom_new_menu');
+
 ?>
