f725121a8d350426d1cb224906050847922940a5
liamvandelangeryt1 <43748516+liamvandelangeryt1@users.noreply.github.com>
2021-03-04 10:54:37 +0100

    details
    singel.php gebruiksvriendelijk gemaakt met home knop.
    archive.php gebruiksvriendelijk gemaakt met home knop.
    404.php gebruiksvriendelijk gemaakt met home knop.

--- wordpress/wp-content/themes/Bikestore/404.php
+++ wordpress/wp-content/themes/Bikestore/404.php
@@ -1,10 +1,16 @@
 <?php get_header() ?>
 
-<h1>U bent verkeerd op onze site terecht gekomen gelieven volgende knop te gebruiken voor u naar de home pagina te begeven.</h1>
-<button class="btn btn-primary o-button" type="submit">
-    Proefrit aanvragen
-</button>
+<div class="container text-center">
+    <h1>Beste gebruiker</h1>
+    <br>
+    <p>U bent verkeerd op onze site terecht gekomen. </p>
+    <p>Gelieve volgende knop te gebruiken voor u naar de home pagina te begeven.</p>
+    <a href="<?php echo get_home_url() ?>">
+        <button class="btn btn-primary o-button" type="submit">
+            Home
+        </button>
+    </a>
 
-
+</div>
 
 <?php get_footer() ?>
